41c8cc79fb7f814b3669609ee2892abe2f765cda
j-o-d-o <johannes_dob@web.de>
2017-12-28 12:48:09 +0100

    fix server jump

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -21,42 +21,13 @@ export class Player {
                 restitution: 0.35
             }, this.scene);
     }
-
-    private isOnGround(): boolean {
-        let isOnGround: boolean = false;
-
-        let minY = this.playerMesh.getBoundingInfo().minimum.y + this.playerMesh.position.y;
-        let pos = this.playerMesh.absolutePosition.clone();
-        pos.y = minY;
-
-        var ray: BABYLON.Ray = new BABYLON.Ray(this.playerMesh.absolutePosition, new BABYLON.Vector3(0, -1, 0));
-
-        let counter: number = 0;
-        let info = this.scene.pickWithRay(ray, (mesh) => {
-            return !(mesh === this.playerMesh);
-        });
-
-        if (info.hit) {
-            let pickedY = info.pickedPoint.y;
-            isOnGround = (pickedY + 0.15) >= minY;
-        }
-
-        return isOnGround;
-    }
  
     public applyMovement(unit_direction: BABYLON.Vector3, force: number) {
         let contactPoint: BABYLON.Vector3 = this.playerMesh.absolutePosition.clone();
         contactPoint.y += 20;
-        if (unit_direction.y === 1) {
-            // Jump
-            if(this.isOnGround()){
-                let direction: BABYLON.Vector3 = unit_direction.multiplyByFloats(force, force, force);
-                this.playerMesh.applyImpulse(direction, contactPoint);
-            }
-        }
-        else {
-            let direction: BABYLON.Vector3 = unit_direction.multiplyByFloats(force, force, force);
-            this.playerMesh.applyImpulse(direction, contactPoint);
-        }
+        
+        let direction: BABYLON.Vector3 = unit_direction.multiplyByFloats(force, force, force);
+        this.playerMesh.applyImpulse(direction, contactPoint);
+        
     }
 }
